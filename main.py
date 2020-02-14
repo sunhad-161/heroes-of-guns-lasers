c25747cfe49c5ff87e52c2514225e3006cd6eb95
sunhad-161 <58877520+sunhad-161@users.noreply.github.com>
2020-02-14 11:22:41 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,59 +1,63 @@
 import pygame
 import props
+import windows
+import music
+
+
+def draw():
+    props.walls.draw(screen)
+    props.all_props.draw(screen)
+    props.en_props.draw(screen)
+    props.player.draw(screen)
+    props.player.update(event)
+    props.all_props.update(event)
+    props.en_props.update(event)
 
 
 pygame.init()
 pygame.mixer.init()
 
-size = width, height = 1280, 720
+size = width, height = 1280, 704
+music.main_menu()
 
-main_menu_music = pygame.mixer.music.load('HateBit-Pixelizer.mp3')
 screen = pygame.display.set_mode(size)
 pygame.mixer.music.play(-1)
 
 running = True
 clock = pygame.time.Clock()
-pl = props.Player(width // 2, height // 2, (255, 0, 0))
-cur = props.Cursor(0, 0, (0, 255, 0))
-bullets = list()
-pygame.mouse.set_visible(False)
+pl = props.Player(width // 2, height // 2)
+start_b = windows.Button(320, 250, 280, 110)
+start_b.set_text('Start')
+window = 'main'
+pygame.mouse.set_visible(True)
+
+# основной цикл
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_UP:
-                pl.vector(0, -9)
-            if event.key == pygame.K_LEFT:
-                pl.vector(-9, 0)
-            if event.key == pygame.K_DOWN:
-                pl.vector(0, 9)
-            if event.key == pygame.K_RIGHT:
-                pl.vector(9, 0)
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_UP:
-                pl.vector(0, 9)
-            if event.key == pygame.K_LEFT:
-                pl.vector(9, 0)
-            if event.key == pygame.K_DOWN:
-                pl.vector(0, -9)
-            if event.key == pygame.K_RIGHT:
-                pl.vector(-9, 0)
-        if event.type == pygame.MOUSEMOTION:
-            cur.move(*event.pos, screen)
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            center = (pl.x + pl.width // 2, pl.y + pl.height // 2)
-            bullets.append(props.Bullet(*center, (200, 200, 0)))
-            b_vector = props.count_vectors(pl.x + pl.width // 2, pl.y + pl.height // 2,
-                                           cur.x + cur.width // 2, cur.y + cur.height // 2,
-                                           12, bullets[-1])
-            if b_vector:
-                bullets[-1].vector(*b_vector)
-    screen.fill((0, 0, 0))
-    pl.move(screen)
-    cur.render(screen)
-    for bul in bullets:
-        bul.check(screen, bullets)
-    clock.tick(80)
+        elif event.type == pygame.MOUSEBUTTONDOWN and window == 'main':
+            if start_b.check(*event.pos):
+                window = 'game'
+                music.game()
+                pygame.mouse.set_visible(False)
+                pygame.mixer.music.play(-1)
+                props.now_room = props.load_level()
+                props.generate_level(props.now_room)
+                cur = props.Cursor(*event.pos)
+                print(props.en_props)
+        elif window == 'game':
+            if event.type == pygame.KEYDOWN or event.type == pygame.KEYUP:
+                pl.update(event)
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                x, y = pl.rect.x + pl.rect.width // 2, pl.rect.y + pl.rect.height // 2
+                props.Bullet(x, y, *event.pos)
+    screen.fill((25, 25, 25))
+    if window == 'game':
+        draw()
+        pl.move()
+    elif window == 'main':
+        start_b.render(screen)
+    clock.tick(60)
     pygame.display.flip()
 pygame.quit()
